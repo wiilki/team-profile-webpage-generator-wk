6b77beb9d933da7691fd19e7df9b33b67c30c6d7
Will <willkim0608@gmail.com>
2022-12-16 15:09:08 -0800

    Add inquirer prompt

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const Employee = require("./lib/Employee");
 
 const addEmployee = [
-    {
-        type: 'input',
-        name: 'empName',
-        message: "Enter employee's name:",
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: "Enter employee's id #:",
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: "Enter employee's email:",
-    }
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'empName',
+                message: "Enter employee's name:",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Enter employee's id #:",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Enter employee's email:",
+            }
+        ])
 ];
