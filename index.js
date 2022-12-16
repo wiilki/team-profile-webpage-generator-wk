c28330d0a92fb7904dce9aa951214aac5a6d726c
Will <willkim0608@gmail.com>
2022-12-16 15:06:38 -0800

    Add employee input prompt

---
+++ index.js
@@ -0,0 +1,19 @@
+const Employee = require("./lib/Employee");
+
+const addEmployee = [
+    {
+        type: 'input',
+        name: 'empName',
+        message: "Enter employee's name:",
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Enter employee's id #:",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter employee's email:",
+    }
+];
