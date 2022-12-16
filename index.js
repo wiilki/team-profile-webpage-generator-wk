a97d54f58430b3283d68fa0e625e64f34a944bc2
Will <willkim0608@gmail.com>
2022-12-16 15:15:08 -0800

    Add addManager and edit addEmployee

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const Employee = require("./lib/Employee");
 
+const addManager = [
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "Enter manager's name:",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Enter manager's id #:",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Enter manager's email:",
+            },
+            {
+                type: 'input',
+                name: 'officeNum',
+                message: "Enter manager's office number:",
+            }
+        ])
+];
+
+
 const addEmployee = [
     inquirer
         .prompt([
             {
                 type: 'input',
-                name: 'empName',
+                name: 'name',
                 message: "Enter employee's name:",
             },
             {
@@ -17,6 +44,12 @@ const addEmployee = [
                 type: 'input',
                 name: 'email',
                 message: "Enter employee's email:",
+            },
+            {
+                type: 'list',
+                name: 'role',
+                message: "Choose employee's role:",
+                choices: ['Engineer', 'Intern']
             }
         ])
 ];
