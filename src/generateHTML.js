c293488516f4dfa3c5049cadc13e364ef25f253e
Will <willkim0608@gmail.com>
2022-12-20 14:39:30 -0800

    Fix HTML template. Fix styling

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,83 +1,84 @@
 const generateManager = manager => {
     return `
-<div class="employee-card">
-<div class="name-title">
-    <h2>${manager.name}</h2>
-    <h2>${manager.role}</h2>
-</div>
-<div class="info">
-    <p>ID: ${manager.id}</p>
-    <p>Email: ${manager.email}</p>
-    <p>Office number: ${manager.officeNumber}</p>
-</div>
+<div class="card" style="width: 18rem;">
+    <div class="card-header">
+        <h2>${manager.name}</h2>
+        <h2>Manager</h2>
+    </div>
+    <ul class="list-group">
+        <li class="list-group-item">ID: ${manager.id}</li>
+        <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
+        <li class="list-group-item">Office number: ${manager.officeNumber}</li>
+    </ul>
 </div>`;
 }
 
 
 const generateEngineer = engineer => {
     return `
-<div class="employee-card">
-<div class="name-title">
-    <h2>${engineer.name}</h2>
-    <h2>${engineer.role}</h2>
-</div>
-<div class="info">
-    <p>ID: ${engineer.id}</p>
-    <p>Email: ${engineer.email}</p>
-    <p>Github: ${engineer.github}</p>
-</div>
+<div class="card" style="width: 18rem;">
+    <div class="card-header">
+        <h2>${engineer.name}</h2>
+        <h2>Engineer</h2>
+    </div>
+    <ul class="list-group">
+        <li class="list-group-item">ID: ${engineer.id}</li>
+        <li class="list-group-item">Email: ${engineer.email}</li>
+        <li class="list-group-item">Github: <a href="https://github.com/${engineer.github}" target="_blank">${engineer.github}</a></li>
+    </ul>
 </div>`;
 }
 
 const generateIntern = intern => {
     return `
-<div class="employee-card">
-<div class="name-title">
-    <h2>${intern.name}</h2>
-    <h2>${intern.role}</h2>
-</div>
-<div class="info">
-    <p>ID: ${intern.id}</p>
-    <p>Email: ${intern.email}</p>
-    <p>School: ${intern.school}</p>
-</div>
+<div class="card" style="width: 18rem;">
+    <div class="card-header">
+        <h2>${intern.name}</h2>
+        <h2>Intern</h2>
+    </div>
+    <ul class="list-group">
+        <li class="list-group-item">ID: ${intern.id}</li>
+        <li class="list-group-item">Email: ${intern.email}</li>
+        <li class="list-group-item">School: ${intern.school}</li>
+    </ul>
 </div>`;
 }
 
 const generateHTML = allEmployees => {
     newArray = [];
-    for (let i = 0; allEmployees.length>i; i++){ 
+    for (let i = 0; allEmployees.length > i; i++) {
         const role = allEmployees[i].getRole();
-        if (role === 'Manager'){ 
+        if (role === 'Manager') {
             newArray.push(generateManager(allEmployees[i]));
         }
-        if (role === 'Engineer'){ 
+        if (role === 'Engineer') {
             newArray.push(generateEngineer(allEmployees[i]));
         }
-        if (role === 'Intern'){ 
+        if (role === 'Intern') {
             newArray.push(generateIntern(allEmployees[i]));
         }
     }
-    const team = newArray.join(''); 
+    const team = newArray.join('');
 
-    return teamHtml(team); 
+    return teamHtml(team);
 }
 
-const teamHtml= (team) =>
-`<!DOCTYPE html>
+const teamHtml = (team) =>
+    `
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
     <title>Team Profile</title>
 </head>
 
 <body>
-    <header class="navbar navbar-dark bg-dark">
+    <header>
         <h1>My Team</h1>
     </header>
     <main>
@@ -85,6 +86,7 @@ const teamHtml= (team) =>
     </main>
 </body>
 
-</html>`
+</html>
+    `
 
 module.exports = generateHTML; 
