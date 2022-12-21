b80932b36df0cfedb8e4a94d527673dec43230b9
Will <willkim0608@gmail.com>
2022-12-21 12:12:03 -0800

    Edit employee test. Test success

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,15 +1,35 @@
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
-    describe("addEmployee", () => {
-      it("should return an employee object containing name, id, and email", () => {
-        const info = {
-            name: 'Dave',
-            id: '23423',
-            email: 'dave@email.com',
-          }
-        const obj = new Employee(info);  
-        expect(obj.info).toBe(info);        
-      });
+      describe("getName", () => {
+        it("should return name", () => {
+            const name = "Dave";
+            const result = new Employee(name);
+            expect(result.getName()).toBe(name);
+        });
+    });
+        
+    describe("getId", () => {
+        it("should return ID", () => {
+            const id = 12312315;
+            const result = new Employee("Dave", id);
+            expect(result.getId()).toBe(id);
+        });
+    });
+        
+    describe("getEmail", () => {
+        it("should return email", () => {
+            const email = "dave@email.com";
+            const result = new Employee("Dave", 12312315, email);
+            expect(result.getEmail()).toBe(email);
+        });
+    });
+        
+    describe("getRole", () => {
+        it("should return role", () => {
+            const role = "Employee";
+            const result = new Employee("Dave", 12312315, "dave@email.com");
+            expect(result.getRole()).toBe(role);
+        });
     });
   });
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,16 +1,32 @@
 const Manager = require("../lib/Manager");
 
 describe("Manager", () => {
-    describe("addManager", () => {
-      it("should return an Manager object containing name, id, email, and office number", () => {
-        const info = {
-            name: 'Dave',
-            id: '23423',
-            email: 'dave@email.com',
-            officeNumber: '2342343433'
-          }
-        const obj = new Manager(info);  
-        expect(obj.info).toBe(info);        
+    describe("getName", () => {
+        it("should return name", () => {
+          const name = 'Dave';
+          const result = new Manager().getName(name); 
+          expect(result.name).toBe(name);        
+        });
       });
-    });
+    describe("getId", () => {
+        it("should return ID", () => {
+          const id = 12312315;
+          const result = new Manager().getId(id); 
+          expect(result).toBe(id);        
+        });
+      });
+      describe("getEmail", () => {
+        it("should return email", () => {
+          const email = "dave@email.com"
+          const result = new Manager().getEmail(email); 
+          expect(result).toBe(email);        
+        });
+      });
+      describe("getOfficeNum", () => {
+        it("should return office number", () => {
+          const officeNum = 1234567890;
+          const result = new Manager().getOfficeNumber(officeNum);
+          expect(result).toBe(officeNum);        
+        });
+      });
   });
